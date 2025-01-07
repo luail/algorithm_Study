9a848fab513e5308227f2a47e3a14318c6529334
kojoonhyuk <kynia0506@naver.com>
2025-01-07 19:36:30 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.51 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        int max1 = numbers[numbers.length-1];
+        int max2 = numbers[numbers.length-2];
+        answer = max1*max2;
+        
+        return answer;
+    }
+}
